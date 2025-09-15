2c631a7dbd654e817ae3eaf3f576c26bf9f46f6f
aiswaryab369 <aiswarya.b@digitalworks.co>
2025-09-15 12:12:43 +0530

    automatically taking post

--- src/main/java/com/example/habito/HabitoApplication.java
+++ src/main/java/com/example/habito/HabitoApplication.java
@@ -12,7 +12,11 @@ public class HabitoApplication {
 		SpringApplication app = new SpringApplication(HabitoApplication.class);
 		String port = System.getenv("PORT"); // Railway dynamic portS
 		if (port != null && !port.isEmpty()) {
+			System.out.println("Using dynamic PORT: " + port);
 			app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		}
+		else {
+			System.out.println("PORT not found, defaulting to 8080");
 		}
 		app.run(args);
 
