d34ee33a54721ed2fbe64ae59644bb2f1bbd9401
aiswaryab369 <aiswarya.b@digitalworks.co>
2025-09-15 11:06:56 +0530

    Frontend Connecting and error fixing

--- src/main/java/com/example/habito/config/WebConfig.java
+++ src/main/java/com/example/habito/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins("http://localhost:8081","https://habito-theta.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
